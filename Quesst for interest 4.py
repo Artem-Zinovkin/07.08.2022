49d1cfaab36f2cd60a19d26ee50d310973c63992
User <a0968064971@gmail.com>
2022-09-07 21:24:29 +0300

    Задание для интереса 4

---
+++ Quesst for interest 4.py
@@ -0,0 +1,8 @@
+a = int (input("введите первое число "))
+b = int (input("введите второе число "))
+print("a=", a)
+print("b=", b)
+print("после изменения")
+a,b= b,a
+print("a=", a)
+print("b=", b)
