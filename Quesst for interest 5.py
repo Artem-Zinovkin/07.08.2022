04c93ce5ef5d85603e17a686b93c3d6dc32e6a74
User <a0968064971@gmail.com>
2022-09-07 21:59:24 +0300

    Задание для интереса 4

---
+++ Quesst for interest 5.py
@@ -0,0 +1,6 @@
+while True:
+    def polin (stroka):
+        if stroka == stroka [::-1]:
+            print("строка полиндром")
+        else:print("Строка не полиндром")
+    polin(stroka = input("введите строку ")
