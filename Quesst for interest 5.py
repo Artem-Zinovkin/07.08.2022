3be63f266fbb163fd23d0e7a3b63bf8ce7fcdf5f
Artem-Zinovkin <113043587+Artem-Zinovkin@users.noreply.github.com>
2022-09-07 22:28:41 +0300

    Add files via upload

---
+++ Quesst for interest 5.py
@@ -0,0 +1,6 @@
+while True:
+    def polin (stroka):
+        if stroka == stroka [::-1]:
+            print("строка палиндром")
+        else:print("Строка не палиндром")
+    polin(stroka = input("введите строку "))
