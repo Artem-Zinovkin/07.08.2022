6b8cdb41872efe10c6a514ff660057803f3791a4
Artem-Zinovkin <113043587+Artem-Zinovkin@users.noreply.github.com>
2022-09-07 22:17:04 +0300

    Delete Quesst for interest 5.py

--- Quesst for interest 5.py
+++
@@ -1,6 +0,0 @@
-while True:
-    def polin (stroka):
-        if stroka == stroka [::-1]:
-            print("строка полиндром")
-        else:print("Строка не полиндром")
-    polin(stroka = input("введите строку ")
